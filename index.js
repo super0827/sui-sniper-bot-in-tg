674d3e19424f0153c604310fbddfbbd3732d2320
super0827 <hightech0827@gmail.com>
2024-10-04 05:48:19 +0900

    fix: version_2

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const axios = require("axios");
 require('dotenv').config();
 const { Telegraf, Markup, Input } = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
-const desText = `
-Welcome to SUI_sniper_bot, @`;
+const welcomeMsg = `🚀 Welcome to SUI_Sniper_Bot, @`;
+const infoMsg = `Our innovative bot is designed to enhance your token management by monitoring new lunched SUI token. 💡\n
+With SUI Sniper Bot, navigating complex decisions becomes effrotless and manage New Tokens easily. 🌟`;
+const guideMsg = `Follow /info to get New Token Info`;
 const cert = "Made with 💖 by Super Sniper who love you";
-const waitMsg = "You'll get Token Info after a new token lunched.\n Thanks";
-var prevPools;
+const waitMsg = "You'll get Token Info when a new token lunched.\nPlease wait...";
+var prevPools = [];
 var prevLen;
 
 const getPools = async () => {
@@ -18,26 +20,24 @@ const notifyPrice = async (chatId) => {
   try {
     const currPools = await getPools();
     const currLen = currPools.length;
-    console.log("none new lunched -> ", currLen);
+    console.log("none new lunched : ", "currLen -> ", currLen, " prevLen -> ", prevLen) ;
     if(currLen > prevLen){
       console.log("Ooohhh new lunched");
       prevLen = currLen;
-
-      //------ compare logic ------
       const newPool = currPools.filter((curr) => !prevPools.some((prev) => curr.symbol == prev.symbol));
-      //---------------------------
-
-      const poolsMessage = `Total Lunched Token Count : \n\t${len}\nName : \n\t${JSON.stringify(newPool.name)}\nSymbol : \n\t${JSON.stringify(newPool.symbol)}\nPrice : \n\t${JSON.stringify(newPool.vol_usd)}\nSwap_account : \n\t${JSON.stringify(newPool.swap_account)}`; // Indent with 2 spaces
-      await bot.telegram.sendMessage(chatId, `Good News:\n\`\`\`New_Lunched_Token_Info\n${poolsMessage}\`\`\``, { parse_mode: 'MarkdownV2' });
+      for(let i = 0; i < newPool.length ; i++){
+        const poolsMessage = `Total Lunched Token Count : \n\t${currLen - newPool.length + i + 1}\nName : \n\t${JSON.stringify(newPool[i].name)}\nSymbol : \n\t${JSON.stringify(newPool[i].symbol)}\nPrice : \n\t${JSON.stringify(newPool[i].vol_usd)}\nSwap_account : \n\t${JSON.stringify(newPool[i].swap_account)}`; // Indent with 2 spaces
+        await bot.telegram.sendMessage(chatId, `Good News:\n\`\`\`New_Lunched_Token_Info\n${poolsMessage}\`\`\``, { parse_mode: 'MarkdownV2' });
+      }
+      prevPools = currPools;
     }
-
   } catch (error) {
     console.error('Error fetching price:', error);
   }
 };
 
 bot.command("start", async (ctx) => {
-  ctx.reply(`${desText}${ctx.from.username}\n${cert}\n\nYou can get Token Info to type "/info"`);
+  ctx.reply(`${welcomeMsg}${ctx.from.username}!\n\n${infoMsg}\n\n${guideMsg}\n\n${cert}`);
 });
 
 bot.command("info", async (ctx) => {
@@ -46,20 +46,8 @@ bot.command("info", async (ctx) => {
   bot.telegram.sendMessage(chatId, `${waitMsg}`);
   const count = await axios.get("https://api-sui.cetus.zone/v2/sui/swap/count");
   prevLen = count?.data?.data?.pools?.length;
-  prevPools = count;
+  prevPools = count?.data?.data?.pools;
   setInterval(() => notifyPrice(chatId), 5000);
 });
-
-// bot.start((ctx) => {
-//   const username = ctx.from.username; // Get the username of the user
-//   const firstName = ctx.from.first_name; // Get the first name of the user
-
-//   // Check if username exists
-//   if (username) {
-//       ctx.reply(`Welcome ${firstName}! Your username is @${username}.`);
-//   } else {
-//       ctx.reply(`Welcome ${firstName}! You don't have a username set.`);
-//   }
-// });
 
 bot.launch();
